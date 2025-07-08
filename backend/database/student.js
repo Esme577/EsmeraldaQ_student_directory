e600272deb8a99c7fa3b6820b9e77e93b07a7a1d
Esme577 <esmeraldaquezada57@gmail.com>
2025-07-07 20:41:12 -0700

    created react templete for frontend and created an empty folder for backend that I then added a folder database for my data base and a folder in database holdeing all my users in a student.js file

---
+++ backend/database/student.js
@@ -0,0 +1,26 @@
+export const students = [
+  {
+    id: 1,
+    name: "Alex Gonzalez",
+    email: "alex@example.com",
+    enrolled: true,
+  },
+  {
+    id: 2,
+    name: "Jose Ortiz",
+    email: "jordan@example.com",
+    enrolled: false,
+  },
+  {
+    id: 3,
+    name: "Tea Melendez",
+    email: "tea@example.com",
+    enrolled: true,
+  },
+  {
+    id: 4,
+    name: "Wren Purcell",
+    email: "wren@example.com",
+    enrolled: true,
+  },
+];
